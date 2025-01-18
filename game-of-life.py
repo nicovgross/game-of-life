e99a03ecfa48e476a4e28944fce6973cfe1db0c7
nicovgross <nicholasvallegross@gmail.com>
2025-01-18 16:41:58 -0300

    modularized main funtion

--- game-of-life.py
+++ game-of-life.py
@@ -2,12 +2,11 @@ import numpy as np
 import sys
 import pygame
 
-WIDTH, HEIGHT = 800, 600  
+WIDTH, HEIGHT = 800, 800  
 GRID_SIZE = 50            
 CELL_SIZE = WIDTH // GRID_SIZE  
-FPS = 24                 
+FPS = 10                 
 
-# Cores
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (50, 50, 50)
@@ -43,14 +42,40 @@ def update_board(prev_board):
             
             live_neighbors = check_neighbors(prev_board, cell)
 
-            if alive and live_neighbors not in [2, 3]:
-                new_board[cell] = 0
-            elif alive and (live_neighbors == 2 or live_neighbors == 3):
+            if alive and live_neighbors in [2, 3]:
                 new_board[cell] = 1
+            elif alive and live_neighbors not in [2, 3]:
+                new_board[cell] = 0
             elif not alive and live_neighbors == 3:
                 new_board[cell] = 1
 
     return new_board
+
+def handle_events(board, running):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and not running:
+            x, y = pygame.mouse.get_pos()
+            cell_x, cell_y = x // CELL_SIZE, y // CELL_SIZE
+            board[cell_y, cell_x] = 1 - board[cell_y, cell_x]
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                running = not running 
+            if event.key == pygame.K_r:
+                board = initialize_board(GRID_SIZE) 
+                running = not running
+    return board, running
+
+def draw_grid(board, screen):
+    screen.fill(BLACK)
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            rect = pygame.Rect(j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            color = WHITE if board[i, j] == 1 else BLACK
+            pygame.draw.rect(screen, color, rect)
+            pygame.draw.rect(screen, GRAY, rect, 1)
 
 def game_of_life():
     pygame.init()
@@ -61,31 +86,12 @@ def game_of_life():
     board = initialize_board(GRID_SIZE)
     running = False  
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and not running:
-                x, y = pygame.mouse.get_pos()
-                cell_x, cell_y = x // CELL_SIZE, y // CELL_SIZE
-                board[cell_y, cell_x] = 1 - board[cell_y, cell_x]
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    running = not running 
-                if event.key == pygame.K_r:
-                    board = initialize_board(GRID_SIZE) 
-                    running = not running
+        board, running = handle_events(board, running)
 
         if running:
             board = update_board(board)
 
-        screen.fill(BLACK)
-        for i in range(GRID_SIZE):
-            for j in range(GRID_SIZE):
-                rect = pygame.Rect(j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-                color = WHITE if board[i, j] == 1 else BLACK
-                pygame.draw.rect(screen, color, rect)
-                pygame.draw.rect(screen, GRAY, rect, 1)
+        draw_grid(board, screen)
 
         pygame.display.flip()
         clock.tick(FPS)
